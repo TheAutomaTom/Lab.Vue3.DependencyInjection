1610bc5b67734dd3897549ac18edad5d02e6a285
Tom Grossi <TheAutomaTom@gmail.com>
2024-05-10 11:27:09 -0500

    Make View's requirements lazier

--- src/App/Views/SomeView.vue
+++ src/App/Views/SomeView.vue
@@ -2,27 +2,27 @@
 import { ref } from "vue";
 import { DependencyFactory } from "../Infra/DependencyFactory";
 import { GetSomeDataRequest } from "@/Core/Features/SomeFeature/GetSomeData/GetSomeDataRequest";
+import type { SomeFeatureCollection } from "@/Core/Features/SomeFeature/SomeFeatureCollection";
 
 console.warn(`BEGIN SomeView initializing`);
 
 // const some$ = useSomeStore();
-const someFeatureCollection = DependencyFactory.SomeFeatureCollectionImplementation();
+let someFeatureCollection: SomeFeatureCollection; // = DependencyFactory.SomeFeatureCollectionImplementation();
 
 const someOutput = ref("Please provide input.");
 const someInput = ref("");
 
 const tryGetData = async() => {
-
+  if(someFeatureCollection == null){
+    someFeatureCollection = DependencyFactory.SomeFeatureCollectionImplementation();
+  }
   console.warn(`BEGIN SomeView.tryGetData`);
-
   if(someInput.value == ""){
     someOutput.value = "Please provide input.";
     return;
   }
 
   const request = new GetSomeDataRequest(someInput.value);
-
-  // var result = await someFeature.getSomeData(request);
   var result = await someFeatureCollection.getSomeDataHandler.handle(request);
   someOutput.value = result.data;
 }
